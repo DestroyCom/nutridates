edb09cf09fb54f157998a68a60d480e9c8c2e534
DestCom <azevedo-a@hotmail.fr>
2021-07-19 22:10:34 +0200

    change security properties

---
+++ src/credentials-firebase.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: process.env.REACT__APP_API_KEY ,
+    authDomain: process.env.REACT__APP_AUTH_DOMAIN ,
+    databaseURL: process.env.REACT__APP_DATABASE_URL ,
+    projectId: process.env.REACT__APP_PROJECT_ID ,
+    storageBucket: process.env.REACT__APP_STORAGE_BUCKET ,
+    messagingSenderId: process.env.REACT__APP_MESSAGING_SENDER_ID ,
+    appId: process.env.REACT__APP_APP_ID ,
+    measurementId: process.env.REACT__APP_MEASUREMENT_ID 
+  };
